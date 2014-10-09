dfb873ef2fe09fd8b21febe3e92fa8c48206d5d3
YuanCheng <367378371@qq.com>
2014-10-09 17:42:15 +0800

    WeChat
    WeChat

---
+++ wcxmpp.h
@@ -0,0 +1,15 @@
+//
+//  wcxmpp.h
+//  WeChat
+//
+//  Created by YC on 14-10-8.
+//  Copyright (c) 2014年 YC. All rights reserved.
+//
+
+#ifndef WeChat_wcxmpp_h
+#define WeChat_wcxmpp_h
+
+
+#endif
+
+#import "XMPP.h"
